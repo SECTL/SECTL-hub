ee26d1f5d4b149447b643c00f357b8d2044c8aea
GitHub Action <action@github.com>
2025-08-16 11:23:24 +0000

    🔄 自动更新图片画廊列表 [skip ci]

--- docs/.vuepress/components/ImageGallery.vue
+++ docs/.vuepress/components/ImageGallery.vue
@@ -131,6 +131,8 @@ const fetchImages = async () => {
     const imageExtensions = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
     const builtinImages = [
       '*东北粗口*.png',
+      '*粤韵风华*_90.png',
+      '*粤韵风华*.png',
       '淦亖你啊.png',
       '拖出去斩了.png',
       '我不管.png'
