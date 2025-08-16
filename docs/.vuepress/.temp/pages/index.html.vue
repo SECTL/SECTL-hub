fdb4444a07785be8ee2b0b3e73716ac1f38692ec
黎泽懿 <lzy.12@foxmail.com>
2025-08-16 16:38:17 +0800

    update

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,7 +0,0 @@
-<template><div><h2 id="乐子展示" tabindex="-1"><a class="header-anchor" href="#乐子展示"><span>乐子展示</span></a></h2>
-<ImageGallery /></div></template>
-
-
-<script setup>
-// 不需要额外的导入，组件已在client.ts中全局注册
-</script>
